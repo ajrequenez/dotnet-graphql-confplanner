671a287cf3ef5d090239f9629b37aadcb316f6ec
James Requenez <jrequenez@Jamess-MacBook-Pro.local>
2022-02-24 12:07:53 -0600

    🎉  chore: initial commit

---
+++ GraphQL/AddSpeakerInput.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ConferencePlanner.GraphQL
+{
+    public record AddSpeakerInput(
+        string Name,
+        string? Bio,
+        string? WebSite);
+}
+
---
+++ GraphQL/Extensions/UseApplicationDbContextAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using ConferencePlanner.GraphQL.Data;
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
+
+namespace ConferencePlanner.GraphQL
+{
+	public class UseApplicationDbContextAttribute: ObjectFieldDescriptorAttribute
+    {
+        public override void OnConfigure(IDescriptorContext context, IObjectFieldDescriptor descriptor, MemberInfo member)
+        {
+            descriptor.UseDbContext<ApplicationDbContext>();
+        }
+    }
+}
+
